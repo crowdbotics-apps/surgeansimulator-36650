bab3872dae768bd55d4c59837fe63bb2c6383ab8
Crowdbotics <team@crowdbotics.com>
2022-08-07 17:28:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "surgeansimulator_36650",
+  url: "https://surgeansimulator_36650.botics.co",
+  api: "https://surgeansimulator_36650.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
